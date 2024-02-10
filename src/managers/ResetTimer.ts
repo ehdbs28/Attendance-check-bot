04ef48392c69c4996ee067bc6281454cd4b8865d
ehdbs28 <doyun5373@naver.com>
2024-02-11 04:01:28 +0900

    출석 현황판 제작

--- src/managers/ResetTimer.ts
+++ src/managers/ResetTimer.ts
@@ -1,18 +1,21 @@
-import { ApplicationCommandDataResolvable, Client, Interaction, User } from "discord.js";
+import { APIEmbed, APIEmbedField, ApplicationCommandDataResolvable, Client, EmbedField, Interaction, User } from "discord.js";
 import Commands from "../commands";
 import {Logger, ILogObj} from "tslog";
 import { SlashCommand } from "../types/slashCommand";
 import dotenv from "dotenv";
 import { UserData } from "../saveData/userData";
+import { EmbedManager } from "./embedManager";
+import { embedManager } from "..";
 dotenv.config();
 
 const log: Logger<ILogObj> = new Logger();
 
 const dataResetHour: number = process.env.dataResetHour === undefined ? 0 : parseInt(process.env.dataResetHour);
+const notificationChannelId: string = process.env.notificationChannelId === undefined ? '' :process.env.notificationChannelId;
 
 export class ResetTimer{
     private client: Client;
-    private lastCheckDate: Date | null;
+    private lastCheckDate: Date;
 
     public StartTimerCheck(){
         this.TimerCheck();
@@ -33,9 +36,7 @@ export class ResetTimer{
         let hours: number = date.getHours();
         
         let hourCheck: boolean = hours === dataResetHour;
-        let alreadyCheckDate: boolean = this.lastCheckDate !== null && 
-            this.lastCheckDate.getMonth() === date.getMonth() &&
-            this.lastCheckDate.getDate() === date.getDate();
+        let alreadyCheckDate: boolean = this.lastCheckDate.getMonth() === date.getMonth() && this.lastCheckDate.getDate() === date.getDate();
 
         if(hourCheck && !alreadyCheckDate){
             this.lastCheckDate = date;
@@ -46,12 +47,46 @@ export class ResetTimer{
         setTimeout(()=>this.TimerCheck(), 1000);
     }
     
-    private SendInformationImbed(){
+    private async SendInformationImbed(){
+        let channel = this.client.channels.cache.get(notificationChannelId) 
+
+        if(!channel || !channel.isTextBased()){
+            log.error("[ResetTimer] Invalid Channel id");
+            return;
+        }
+
+        var userFields : APIEmbedField[] = await this.getUserFields();
+
+        channel.send({ embeds: [embedManager.createEmbed({
+            title: `${this.lastCheckDate?.getMonth() + 1}/${this.lastCheckDate?.getDate() - 1} 출석 기록을 마무리 합니다.`,
+            color: "#205576",
+            desc: "오늘 하루 출석 현황을 보여드리겠습니다.",
+            fields: userFields
+        })]});
+    }
+
+    private async getUserFields() : Promise<APIEmbedField[]> {
+        let userFields: APIEmbedField[] = [
+            { name: "\t", value: "\t", inline: true },
+            { name: "\t", value: "\t", inline: true },
+            { name: "\t", value: "\t", inline: true },
+            { name: "\t", value: "\t", inline: false },
+        ];
         
+        for (const data of UserData.data) {
+            let user = await this.client.users.fetch(data.id);
+            let date = new Date(data.attendanceStartTime === null ? 0 : data.attendanceStartTime);
+            userFields.push({ name: "유저", value: `${user.displayName}`, inline: true });
+            userFields.push({ name: "출석", value: `${data.attendance ? "완료" : "미달"}`, inline: true });
+            userFields.push({ name: "출석 시각", value: `${data.attendance ? date.getHours() : '--'} : ${data.attendance ? date.getMinutes() : '--'}`, inline: true });
+            userFields.push({ name: "\t", value: "\t", inline: false });
+        }
+
+        return userFields;
     }
 
     constructor(client: Client){
         this.client = client;
-        this.lastCheckDate = null;
+        this.lastCheckDate = new Date(0);
     }
 }
